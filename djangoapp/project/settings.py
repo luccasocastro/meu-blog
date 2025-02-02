39089c40a458efd51492737596aa8470bced9e4e
luccasocastro <emaildocastro.adm01@gmail.com>
2025-02-02 19:12:24 -0300

    chore: setuping docker containers

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DG_ENGINE', 'change-me'),
+        'ENGINE': os.getenv('DB_ENGINE', 'change-me'),
         'NAME': os.getenv('POSTGRES_DB', 'change-me'),
         'USER': os.getenv('POSTGRES_USER', 'change-me'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'change-me'),
